c103a3acd978738a7df9d1400dbe7b188f3cfdf8
saritaa620 <selhichou@alumnat.copernic.cat>
2025-05-20 12:51:33 +0200

    Actualització dades de l’anime: dragon ball

--- ex.py
+++ ex.py
@@ -1,7 +1,11 @@
 {
-  "titol": "Buzhuo Ni de She Nian Hao Yun",
-  "sinopsis": "A special video to celebrate the 2025 Chinese New Year, the Year of the Snake.",
-  "puntuacio": None,
-  "episodis": 1,
-  "url": "https://myanimelist.net/anime/60848/Buzhuo_Ni_de_She_Nian_Hao_Yun",
+  "titol": "Dragon Ball",
+  "sinopsis": "Gokuu Son is a young boy who lives in the woods all alone—that is, until a girl named Bulma runs into him in her search for a set of magical objects called the "Dragon Balls." Since the artifacts are said to grant one wish to whoever collects all seven, Bulma hopes to gather them and wish for a perfect boyfriend. Gokuu happens to be in possession of a dragon ball, but unfortunately for Bulma, he refuses to part ways with it, so she makes him a deal: he can tag along on her journey if he lets her borrow the dragon ball's power. With that, the two set off on the journey of a lifetime.
+
+They don't go on the journey alone. On the way, they meet the old Muten-Roshi and wannabe disciple Kuririn, with whom Gokuu trains to become a stronger martial artist for the upcoming World Martial Arts Tournament. However, it's not all fun and games; the ability to make any wish come true is a powerful one, and there are others who would do much worse than just wishing for a boyfriend. To stop those who would try to abuse the legendary power, they train to become stronger fighters, using their newfound strength to help the people around them along the way.
+
+[Written by MAL Rewrite]",
+  "puntuacio": 7.98,
+  "episodis": 153,
+  "url": "https://myanimelist.net/anime/223/Dragon_Ball",
 }
