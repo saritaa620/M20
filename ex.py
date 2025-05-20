5f0dfcde464ef1347693877ca6e173699a3bab00
saritaa620 <selhichou@alumnat.copernic.cat>
2025-05-20 12:51:06 +0200

    Actualització dades de l’anime: arbok

--- ex.py
+++ ex.py
@@ -1,10 +1,7 @@
 {
-  "titol": "Kaichuu!",
-  "sinopsis": "Tachikawa Tetsuya is a freshman at a new high school and he wants to become a great archer, like his late grandfather, Fudou.
-
-One day, a new transfer student joins his class. In a surprise to everyone, it is a cute girl and not a guy like the rumors were saying... The name is Sasaki Gonzaburou, and... "she"... or maybe "he"... well, this person has some "little" secrets.",
-  "puntuacio": 5.79,
-  "episodis": 4,
-  "url": "https://myanimelist.net/anime/9205/Kaichuu",
-  "any_estrenat": "None"
+  "titol": "Buzhuo Ni de She Nian Hao Yun",
+  "sinopsis": "A special video to celebrate the 2025 Chinese New Year, the Year of the Snake.",
+  "puntuacio": None,
+  "episodis": 1,
+  "url": "https://myanimelist.net/anime/60848/Buzhuo_Ni_de_She_Nian_Hao_Yun",
 }
