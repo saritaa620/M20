c8dc9168a19477583ee372279e3b068ba9f5b0ca
saritaa620 <selhichou@alumnat.copernic.cat>
2025-05-20 21:31:58 +0200

    Dades actualitzades de l'anime: pikachu

--- ex.py
+++ ex.py
@@ -1,11 +1,7 @@
 {
-  "titol": "One Piece Movie 01",
-  "sinopsis": "Many years ago, Woonan, a legendary pirate, plundered one-third of the world's gold and stashed it away on his secret island shrouded in mystery. 
-
-In the present, Luffy and the rest of the Straw Hats continue on their journey to the Grand Line. They are robbed by a group of bandits. Led by their captain, El Drago, these same bandits are headed towards Woonan's famed island. Even though the Straw Hat Pirates must now recover their stolen treasure, they have gained another objective: to discover the lost island, its treasures, and learn about the legend of Woonan.
-
-[Written by MAL Rewrite]",
-  "puntuacio": 7.09,
+  "titol": "Pokemon: Pikachu, Kore Nan no Kagi?",
+  "sinopsis": "A thrilling tale in which a group of innocent pokemon get sucked into a world full of keys only to find themselves trapped. Will they ever escape?",
+  "puntuacio": 6.33,
   "episodis": 1,
-  "url": "https://myanimelist.net/anime/459/One_Piece_Movie_01",
+  "url": "https://myanimelist.net/anime/21571/Pokemon__Pikachu_Kore_Nan_no_Kagi"
 }
