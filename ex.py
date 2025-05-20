9f7c1c7a1fb1db81e0173790c5ca245a4c918c9a
saritaa620 <selhichou@alumnat.copernic.cat>
2025-05-20 21:32:06 +0200

    Dades actualitzades de l'anime: naruto

--- ex.py
+++ ex.py
@@ -1,7 +1,11 @@
 {
-  "titol": "Pokemon: Pikachu, Kore Nan no Kagi?",
-  "sinopsis": "A thrilling tale in which a group of innocent pokemon get sucked into a world full of keys only to find themselves trapped. Will they ever escape?",
-  "puntuacio": 6.33,
-  "episodis": 1,
-  "url": "https://myanimelist.net/anime/21571/Pokemon__Pikachu_Kore_Nan_no_Kagi"
+  "titol": "Naruto",
+  "sinopsis": "Moments before Naruto Uzumaki's birth, a huge demon known as the Nine-Tailed Fox attacked Konohagakure, the Hidden Leaf Village, and wreaked havoc. In order to put an end to the demon's rampage, the leader of the village, the Fourth Hokage, sacrificed his life and sealed the monstrous beast inside the newborn Naruto.
+
+In the present, Naruto is a hyperactive and knuckle-headed ninja growing up within Konohagakure. Shunned because of the demon inside him, Naruto struggles to find his place in the village. His one burning desire to become the Hokage and be acknowledged by the villagers who despise him. However, while his goal leads him to unbreakable bonds with lifelong friends, it also lands him in the crosshairs of many deadly foes.
+
+[Written by MAL Rewrite]",
+  "puntuacio": 8.01,
+  "episodis": 220,
+  "url": "https://myanimelist.net/anime/20/Naruto"
 }
